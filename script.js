0b492b0513b20857eda72c3c3816686cd0ac5202
ゆる <yuru4c@gmail.com>
2019-01-08 07:08:44 +0900

    Update script.js

--- script.js
+++ script.js
@@ -932,8 +932,6 @@ var jsont; // JSONPコールバック関数公開用
 		logTexts[i] = logText;
 	}
 	
-	load();
-	
 	function init() {
 		start(); // サーバ時刻取得開始
 		tick();
@@ -944,6 +942,8 @@ var jsont; // JSONPコールバック関数公開用
 	
 	$.onreadystatechange = function () {
 		this.onreadystatechange = null;
+		
+		load();
 		
 		// head 要素取得
 		head = $.getElementsByTagName('head')[0];
